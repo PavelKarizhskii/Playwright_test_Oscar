a9d795f019a86b1ab4200ef271a833a25bfc23e7
PavelKarizhskii <99598143+PavelKarizhskii@users.noreply.github.com>
2023-01-25 14:07:08 +0400

    add test_main_page

---
+++ Tests/test_open_page.py
@@ -0,0 +1,24 @@
+import time
+
+import pytest
+import time
+
+import playwright
+from playwright.sync_api import Playwright, sync_playwright, expect
+import pytest_playwright
+
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+def test_page_open(page):
+
+    page.goto('https://demo.playwright.dev/todomvc/#/')
+    assert page.url == 'https://demo.playwright.dev/todomvc/#/'
+    print(page.url)
+    field = page.locator('[placeholder="What needs to be done?"]')
+    field.fill("Закончить курс по playwright")
+    field.press('Enter')
+    field.fill("Добавить в резюме, что умею автоматизировать")
+    field.press('Enter')
+    todo_item = page.locator('[data-testid="todo-item"]')
+    time.sleep(5)
